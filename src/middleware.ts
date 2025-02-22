7807469909d4af7f50fa6c852be681545dd01717
Zeni <mailzeni42@gmail.com>
2025-02-22 20:25:11 +0530

    middleware added

---
+++ src/middleware.ts
@@ -0,0 +1,26 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+export function middleware(request: NextRequest) {
+    const path = request.nextUrl.pathname
+
+    const isPublicPath = path === '/sign-in' || path === '/sign-up' || path === '/'
+    const token = request.cookies.get("token")?.value
+
+    if (!isPublicPath && !token) {
+        return NextResponse.redirect(new URL('/sign-in', request.url))
+    }
+
+    if (isPublicPath && token) {
+        return NextResponse.redirect(new URL('/home', request.url))
+    }
+}
+
+export const config = {
+    matcher: [
+        '/',
+        '/sign-in',
+        '/sign-up',
+        '/home'
+    ]
+}
